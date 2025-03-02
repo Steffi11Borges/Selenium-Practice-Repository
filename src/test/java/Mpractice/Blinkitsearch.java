7ed0321798b272d71cf0741b6f35d3ed8dbde211
Steffi11Borges <138497942+Steffi11Borges@users.noreply.github.com>
2025-03-03 01:52:05 +0530

    Update Blinkitsearch.java

--- src/test/java/Mpractice/Blinkitsearch.java
+++ src/test/java/Mpractice/Blinkitsearch.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Blinkitsearch {
-
+//PULL BACK
 	public static void main(String[] args) {
 		WebDriver driver;
 		WebDriverManager.edgedriver().setup();
