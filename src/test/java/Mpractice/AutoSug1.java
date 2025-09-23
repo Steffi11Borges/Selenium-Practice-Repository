7af329420a4b760a8521201571cb8a2f2c06c43b
Steffi11Borges <138497942+Steffi11Borges@users.noreply.github.com>
2025-09-23 12:13:31 +0530

    Update AutoSug1.java

--- src/test/java/Mpractice/AutoSug1.java
+++ src/test/java/Mpractice/AutoSug1.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AutoSug1 {
-//coomit from jira
+//commit from jira
 	public static void main(String[] args) throws Throwable {
 		WebDriver driver;
 		WebDriverManager.edgedriver().setup();
@@ -35,4 +35,5 @@ public class AutoSug1 {
 	}
 
 }
+
 
